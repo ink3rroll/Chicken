f17b1f23d19f40652b52c8967f3dcc42eb140974
ink3rroll <ink3rroll@gmail.com>
2023-10-23 22:28:01 +0800

    Fixed some bugs and exception handling. Added new features to sellers. Created a main class called Shop

--- eComClass.py
+++ eComClass.py
@@ -24,6 +24,7 @@ class Seller:
         self.__SellerID = f"#S{Seller.numSeller}"
         self.products = []
         self.__profits = 0
+        self.__transactions = []
         Seller.numSeller += 1
 
     def setPassword(self, password):
@@ -40,6 +41,16 @@ class Seller:
     
     def addProfit(self, val):
         self.__profits += val
+    
+    def viewTransactions(self):
+        if len(self.__transactions) == 0:
+            print("No transaction records.")
+        for i in self.__transactions:
+            i.printTransactionDetails()
+    
+    def addTransaction(self, tr):
+        self.__transactions.append(tr)
+        
 
     def AddProduct(self):
         productName = input("Product: ")
@@ -49,6 +60,22 @@ class Seller:
         product = Product(productName, productDescription, productPrice, productQuantity)
         product.seller = self
         self.products.append(product)
+    
+    def addStocks(self):
+        self.printProducts()
+        if len(self.products) == 0:
+            return
+        try:    
+            product = int(input("Change price\nProduct ID: "))
+            for i in range(0, len(self.products)):
+                if product == self.products[i].productID:
+                    addedStock = float(input("Add stocks: "))
+                    self.products[i].stockQuantity += addedStock
+                    return
+                
+            addedStock += True
+        except:
+            print("Invalid product ID.")
     
     def printProducts(self):
         if len(self.products) == 0:
@@ -61,24 +88,35 @@ class Seller:
         self.printProducts()
         if len(self.products) == 0:
             return
-        product = input("Remove: ")
-        for i in range(0, len(self.products)):
-            if product == self.products[i].name:
-                self.products.remove(self.products[i])
+        try:    
+            product = int(input("Product ID: "))
+            for i in range(0, len(self.products)):
+                if product == self.products[i].productID:
+                    self.products.remove(self.products[i])
+                    return
+            newPrice += True
+        except:
+            print("Invalid product ID.")
+
     
     def setPrice(self):
         self.printProducts()
         if len(self.products) == 0:
             return
-        product = input("Change price\nProduct Name: ")
-        newPrice = float(input("New price: "))
-        for i in range(0, len(self.products)):
-            if product == self.products[i].name:
-                self.products[i].price = newPrice
-    
+        try:    
+            product = int(input("Change price\nProduct ID: "))
+            for i in range(0, len(self.products)):
+                if product == self.products[i].productID:
+                    newPrice = float(input("New price: "))
+                    self.products[i].price = newPrice
+                    return
+            newPrice += True
+        except:
+            print("Invalid product ID.")
+        
     def Deposit(self):
         money = float(input("Deposit amount: "))
-        self.__profits = money
+        self.addProfit(money)
 
 class Buyer:
     numBuyers = 0
@@ -152,6 +190,7 @@ class Buyer:
             i.seller.addProfit(i.price * self.__cart[i])
             record = Transaction(i, self, self.__cart[i])
             self.__transactions.append(record)
+            i.seller.addTransaction(record)
             print(f"Bought {i.name}")
         self.emptyCart()
     
@@ -173,5 +212,192 @@ class Transaction:
     def printTransactionDetails(self):
         print(self.TransactionID, " | ", self.product.name, " | Quantity: ", self.productQuantity, " | ", "S: ", self.product.seller.name, "| B: ", self.buyer.name, " |")
 
+class Shop:
+    def __init__(self):
+        self.buyers = []
+        self.sellers = []
+        self.products = []
+        self.currentAccount = None
 
+
+    def checkStocks(self):
+        for i in self.sellers:
+            for j in i.products:
+                if j.stockQuantity == 0:
+                    i.products.remove(j)
+
+    def viewProducts(self):
+        for i in self.sellers:
+            for j in i.products:
+                j.printDetails()
+
+
+    def updateProducts(self):
+        for i in self.sellers:
+            for j in i.products:
+                self.products = list(self.products)
+                self.products.append(j)
+                self.products = set(self.products)
+        self.checkStocks()
+
+    def createBuyerAccount(self):
+
+        name = input("Name: ")
+        password = input("Password: ")
+        while len(password) < 8:
+            print("Password should be at least 8 characters.")
+            password = input("Password: ")
+        account = Buyer(name)
+        account.setPassword(password)
+        self.buyers.append(account)
+        self.currentAccount = account
+
+    def createSellerAccount(self):
+
+        name = input("Name: ")
+        password = input("Password: ")
+        while len(password) < 8:
+            print("Password should be at least 8 characters.")
+            password = input("Password: ")
+        account = Seller(name)
+        account.setPassword(password)
+        self.sellers.append(account)
+        print("Account Succesfully Created!")
+        self.currentAccount = account
+
+    def loginBuyer(self):
+        while(True):
+            name = input("Name: ")
+            password = input("Password: ")
+            for i in self.buyers:
+                if name == i.name:
+                    if i.getPassword() == password:
+                        self.currentAccount = i
+                        return True
+            print("Incorrect information.")
+            tr = input("Exit? Y/N: ").upper()
+            if tr == 'Y':
+                break
+
+    def loginSeller(self):
+        while(True):
+            name = input("Name: ")
+            password = input("Password: ")
+            for i in self.sellers:
+                if name == i.name:
+                    if i.getPassword() == password:
+                        self.currentAccount = i
+                        return True
+            print("Incorrect information.")
+            tr = input("Exit? Y/N: ").upper()
+            if tr == 'Y':
+                break
+
+    def mainMenu(self):
+        Login_SignUp = input("1 Create an account\n2 Login\n3 Quit\n>>")
+        while Login_SignUp not in ('1', '2', '3'):
+            Login_SignUp = input("1 Create an account\n2 Login\n3 Quit\n>>")
+        if Login_SignUp == '3':
+            exit()
+        
+        Buyer_Seller = input("1 Buyer\n2 Seller\n3 Back\n>>")
+        while Buyer_Seller != '1' and Buyer_Seller != '2' and Buyer_Seller != '3':
+            Buyer_Seller = input("1 Buyer\n2 Seller\n3 Back\n>>")
+        if Buyer_Seller == '3':
+            return
+        
+        if Login_SignUp == '1' and Buyer_Seller == '1':
+            self.createBuyerAccount()
+        elif Login_SignUp == '1' and Buyer_Seller == '2':
+            self.createSellerAccount()
+        elif Login_SignUp == '2' and Buyer_Seller == '1':
+            self.loginBuyer()
+        
+
+        elif Login_SignUp == '2' and Buyer_Seller == '2':
+            self.loginSeller()
+
+    def sellerMenu(self):
+        while(True):
+            self.updateProducts()
+            print(f"\nName: {self.currentAccount.name}\nCredits: Php{self.currentAccount.getCredits()}")
+            action = input("1 View your products\n2 Add Product\n3 Add Stocks\n4 Remove Product\n5 Set product price\n6 Deposit\n7 View transactions\n8 Logout\n>>")
+            while action not in ('1', '2', '3', '4', '5', '6', '7', '8'):
+                action = input("1 View your products\n2 Add Product\n3 Add Stocks\n4 Remove Product\n5 Set product price\n6 Deposit\n7 View transactions\n8 Logout\n>>")
+            
+            if action == '1':
+                self.currentAccount.printProducts()
+            
+            elif action == '2':
+                self.currentAccount.AddProduct()
+            
+            elif action == '3':
+                self.currentAccount.addStocks()
+                
+            elif action == '4':
+                self.currentAccount.removeProduct()
+            
+            elif action == '5':
+                self.currentAccount.setPrice()
+            
+            elif action == '6':
+                self.currentAccount.Deposit()
+                
+            elif action == '7':
+                self.currentAccount.viewTransactions()
+
+            elif action == '8':
+                self.currentAccount = None
+                break
+
+
+    def buyerMenu(self):
+        while(True):
+            self.updateProducts()
+            print(f"Name: {self.currentAccount.name}\nCredits: Php{self.currentAccount.getCredits()}")
+            action = input("1 View products\n2 Add to Cart\n3 Remove from Cart\n4 View Cart\n5 Deposit\n6 Checkout\n7 View Transactions\n8 Logout\n>>")
+            while action not in ('1', '2', '3', '4', '5', '6', '7', '8'):
+                action = input("1 View products\n2 Add to Cart\n3 Remove from Cart\n4 View Cart\n5 Deposit\n6 Checkout\n7 View Transactions\n8 Logout\n>>")
+            
+            if action == '1':
+                self.viewProducts()
+            
+            elif action == '2':
+                try:
+                    id = int(input("Product ID: "))
+                    for i in self.products:
+                        if i.productID == id:
+                            print(i.name)
+                            self.currentAccount.addToCart(i)       
+                except:
+                    print("Invalid product ID.")
+            elif action == '3':
+                self.currentAccount.removeFromCart()
+            elif action == '4':
+                self.currentAccount.printCart()
+            
+            elif action == '5':
+                self.currentAccount.Deposit()
+            
+            elif action == '6':
+                self.currentAccount.checkOut()
+            
+            elif action == '7':
+                self.currentAccount.viewTransactions()
+            
+            else:
+                self.currentAccount = None
+                break
+
+    def run(self):
+        while(True):
+            self.updateProducts()
+            if self.currentAccount == None:
+                self.mainMenu()
+            
+            if type(self.currentAccount) == Seller:
+                self.sellerMenu()
+            
+            elif type(self.currentAccount) == Buyer:
+                self.buyerMenu()
 
